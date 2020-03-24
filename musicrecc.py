9b22901d9bca49e0dc76a5a512b0d2f7b1f656a4
AlexK <alexkizia@gmail.com>
2020-03-24 17:40:08 +0000

    Remove the context column once the pre-filtering is complete to ensure less costly operations in shaping etc.

--- musicrecc.py
+++ musicrecc.py
@@ -9,7 +9,7 @@ import random
 import json
 
 app = Flask(__name__)
-user = 1
+user = 1001
 user_songs = []  # stores song IDs that the user has already rated
 
 songs_df = []
@@ -43,13 +43,16 @@ def read_data(d_songs, d_ratings):
 # context as the current user is searchings
 def filter_df(landscape_value):
     global ratings_df
+    # remove all the ratings not made in the same context
     ratings_df = ratings_df[ratings_df.Landscape == landscape_value]
+    # drop the context column because pre-filtering is now complete
+    ratings_df = ratings_df.drop('Landscape', 1)
 
 
 # making the recommendations matrix, fill the rest with 0s
 def create_matrix(ratings):
     recommendation_matrix = ratings.pivot(
-        index="User_ID", columns="Book_ID", values="Rating"
+        index="User_ID", columns="Song_ID", values="Rating"
     ).fillna(0)
     return recommendation_matrix
 
@@ -63,7 +66,7 @@ def demean_data(ratings):
 
 
 def recommend_books(
-    predictions_df, userID, books_df, original_ratings_df, num_recommendations=15
+    predictions_df, userID, songs_df, original_ratings_df, num_recommendations=50
 ):
     user_row_number = userID
     sorted_user_predictions = predictions_df.iloc[user_row_number].sort_values(
@@ -72,7 +75,7 @@ def recommend_books(
 
     user_data = original_ratings_df[original_ratings_df.User_ID == (userID)]
     user_full = user_data.merge(
-        books_df, how="left", left_on="Book_ID", right_on="Book_ID"
+        songs_df, how="left", left_on="Song_ID", right_on="Song_ID"
     ).sort_values(["Rating"], ascending=False)
     print("User {0} has already rated {1} books.".format(userID, user_full.shape[0]))
     print(
@@ -82,12 +85,12 @@ def recommend_books(
     )
 
     recommendations = (
-        books_df[~books_df["Book_ID"].isin(user_full["Book_ID"])]
+        songs_df[~songs_df["Song_ID"].isin(user_full["Song_ID"])]
         .merge(
             pd.DataFrame(sorted_user_predictions).reset_index(),
             how="left",
-            left_on="Book_ID",
-            right_on="Book_ID",
+            left_on="Song_ID",
+            right_on="Song_ID",
         )
         .rename(columns={user_row_number: "Predictions"})
         .sort_values("Predictions", ascending=False)
@@ -120,12 +123,14 @@ def getDuration():
 
 @app.route("/myrecc", methods=["GET"])
 def my_recc():
-
+    # create the matrix
     R_df = create_matrix(ratings_df)
+
+    # demean the data
     R_demeaned, user_ratings_mean = demean_data(R_df)
 
     # singular value decomposition
-    U, sigma, Vt = svds(R_demeaned, k=50)
+    U, sigma, Vt = svds(R_demeaned, k=20)
     sigma = np.diag(sigma)
 
     # making predictions from decomposed matrices
@@ -135,58 +140,14 @@ def my_recc():
     preds_df = pd.DataFrame(all_user_predicted_ratings, columns=R_df.columns)
 
     already_rated, predictions = recommend_books(
-        preds_df, user, books_df, ratings_df, 10
+        preds_df, user, songs_df, ratings_df, 10
     )
 
-    recommended_books = already_rated.head(10)
-    books_json = recommended_books.to_json(orient="records")
-    user_info = {"user_ID": user, "rec_books": books_json}
+    recommended_songs = already_rated.head(10)
+    songs_json = recommended_songs.to_json(orient="records")
+    user_info = {"user_ID": user, "rec_songs": songs_json}
 
     return json.dumps(user_info)
-
-
-@app.route("/rate", methods=["GET"])
-def rate():
-    book_ids = []
-    for i in range(0, 15):
-        val = random.randint(1, 10000)
-        if (val in book_ids) or (val in user_books):
-            break
-        book_ids.append(val)
-
-    book_names = []
-    book_images = []
-    book_authors = []
-
-    books_df.set_index("Book_ID")
-    for book_id in book_ids:
-        book_names.append(books_df.loc[book_id]["Title"])
-        book_images.append(books_df.loc[book_id]["Image"])
-        book_authors.append(books_df.loc[book_id]["Authors"])
-
-    books = {
-        "book_IDs": book_ids,
-        "book_titles": book_names,
-        "book_images": book_images,
-        "book_authors": book_authors,
-    }
-    return json.dumps(books)
-
-
-@app.route("/addRating", methods=["POST"])
-def addRating():
-    rating = int(request.form["rating"])
-    book = int(request.form["book_id"])
-    user_books.append(book)
-
-    arr = ["User_ID", "Book_ID", "Rating"]
-    temp = pd.DataFrame([[user, book, rating]], columns=arr)
-
-    global ratings_df
-
-    ratings_df_new = ratings_df.append(temp, ignore_index=True)
-    ratings_df = ratings_df_new
-    return "success"
 
 
 if __name__ == "__main__":
